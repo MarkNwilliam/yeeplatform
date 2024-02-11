acb587c5fddcc0416c11997173a5c36f20528198
iMac <imac@iMacs-iMac.local>
2024-02-12 00:44:25 +0300

    drawer fixed

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { XMarkIcon, HomeIcon, MagnifyingGlassIcon, BookOpenIcon, SpeakerWaveIcon, DocumentTextIcon } from '@heroicons/react/24/outline';
 import { Link } from 'react-router-dom';
 
-function Sidebar({ sidebarOpen, setSidebarOpen }) {
+function Sidebar({ sidebarOpen, setSidebarOpen, handleSectionClick, selectedSection }) {
     const currentYear = new Date().getFullYear();
   return (
     <aside className={`fixed inset-y-0 left-0 transform ${sidebarOpen ? 'translate-x-0' : '-translate-x-full'} lg:relative lg:translate-x-0 transition duration-200 ease-in-out bg-white w-64 z-20 shadow overflow-y-auto`}>
@@ -16,22 +16,22 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
         <img src="https://assets-hfbubwfaacbch3e0.z02.azurefd.net/assets/images/Y.webp" alt="Logo" className="h-16 w-16 hover:scale-110 transition-transform duration-300" />
       </div>
       <nav className="flex flex-col justify-between h-full">
-        <Link to="/" className="flex items-center py-3 px-4 rounded transition duration-200 hover:bg-yellow-500 hover:text-white text-gray-700 no-underline">
+        <Link to="/" onClick={() => handleSectionClick('/')} className="flex items-center py-3 px-4 rounded transition duration-200 hover:bg-yellow-500 hover:text-white text-gray-700 no-underline">
           <HomeIcon className="h-6 w-6 mr-3 text-yellow-600" /> Home
         </Link>
-        <Link to="/search" className="flex items-center py-3 px-4 rounded transition duration-200 hover:bg-yellow-500 hover:text-white text-gray-700 no-underline">
+        <Link to="/search" onClick={() => handleSectionClick('search')} className="flex items-center py-3 px-4 rounded transition duration-200 hover:bg-yellow-500 hover:text-white text-gray-700 no-underline">
           <MagnifyingGlassIcon className="h-6 w-6 mr-3 text-yellow-600" /> Search
         </Link>
-        <Link to="/ebooks" className="flex items-center py-3 px-4 rounded transition duration-200 hover:bg-yellow-500 hover:text-white text-gray-700 no-underline">
+        <Link to="/ebooks" onClick={() => handleSectionClick('ebooks')} className="flex items-center py-3 px-4 rounded transition duration-200 hover:bg-yellow-500 hover:text-white text-gray-700 no-underline">
           <BookOpenIcon className="h-6 w-6 mr-3 text-yellow-600" /> Ebooks
         </Link>
-        <Link to="/audiobooks" className="flex items-center py-3 px-4 rounded transition duration-200 hover:bg-yellow-500 hover:text-white text-gray-700 no-underline">
+        <Link to="/audiobooks" onClick={() => handleSectionClick('audiobooks')} className="flex items-center py-3 px-4 rounded transition duration-200 hover:bg-yellow-500 hover:text-white text-gray-700 no-underline">
           <SpeakerWaveIcon className="h-6 w-6 mr-3 text-yellow-600" /> Audiobooks
         </Link>
-        <Link to="/audiochapters" className="flex items-center py-3 px-4 rounded transition duration-200 hover:bg-yellow-500 hover:text-white text-gray-700 no-underline">
+        <Link to="/audiochapters" onClick={() => handleSectionClick('audiochapters')} className="flex items-center py-3 px-4 rounded transition duration-200 hover:bg-yellow-500 hover:text-white text-gray-700 no-underline">
           <SpeakerWaveIcon className="h-6 w-6 mr-3 text-yellow-600" /> Audio Chapters
         </Link>
-        <Link to="/chapters" className="flex items-center py-3 px-4 rounded transition duration-200 hover:bg-yellow-500 hover:text-white text-gray-700 no-underline">
+        <Link to="/chapters" onClick={() => handleSectionClick('chapters')} className="flex items-center py-3 px-4 rounded transition duration-200 hover:bg-yellow-500 hover:text-white text-gray-700 no-underline">
           <DocumentTextIcon className="h-6 w-6 mr-3 text-yellow-600" /> Chapters
         </Link>
         {/* Add more links as needed */}
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from 'react';
-import { Routes, Route , Link, useNavigate} from 'react-router-dom';
-import { Bars3Icon, EllipsisVerticalIcon  } from '@heroicons/react/24/outline';
+import { Routes, Route, Link, useNavigate } from 'react-router-dom';
+import { Bars3Icon, EllipsisVerticalIcon } from '@heroicons/react/24/outline';
 import Sidebar from '../components/Sidebar';
 import Intro from "../components/intro";
 import Scontent from "../components/Scontent";
@@ -18,91 +18,96 @@ import AudioChapters from './AudioChapters';
 import { Helmet } from 'react-helmet';
 
 function Home() {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
-  const [menuOpen, setMenuOpen] = useState(false);
-  const menuIconRef = useRef(null);
-  const navigate = useNavigate();
+    const [sidebarOpen, setSidebarOpen] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [selectedSection, setSelectedSection] = useState(''); // New state variable
+    const menuIconRef = useRef(null);
+    const navigate = useNavigate();
 
-  const { user, logout, isAuthor } = useAuth(); // Use the useAuth function
+    const { user, logout, isAuthor } = useAuth(); // Use the useAuth function
 
-  const handleLogout = () => {
-    logout(); // Logout function from AuthContext
-  };
+    const handleLogout = () => {
+        logout(); // Logout function from AuthContext
+    };
 
-  
-  const handleMyAccountClick = () => {
-    // Navigate to the dashboard if the user is an author, otherwise go to the account page
-    if (isAuthor) {
-      navigate('/dashboard');
-    } else {
-      navigate('/myaccount');
-    }
-  };
+    const handleMyAccountClick = () => {
+        // Navigate to the dashboard if the user is an author, otherwise go to the account page
+        if (isAuthor) {
+            navigate('/dashboard');
+        } else {
+            navigate('/myaccount');
+        }
+    };
 
-   // Function to toggle menu popup
-   const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
+    const handleSectionClick = (section) => {
+        setSelectedSection(section);
+        setSidebarOpen(false); // Close the sidebar when a new section is selected
+    };
 
-  return (
-<div className="flex h-screen bg-gray-100">
-      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+    // Function to toggle menu popup
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
 
-      <div className="flex-1 flex flex-col overflow-hidden">
-      <header className="flex justify-between items-center p-4 text-white z-10" style={{ backgroundColor: '#FFDE59' }}>
-          {/* Hamburger Icon */}
-          <button onClick={() => setSidebarOpen(!sidebarOpen)} className={`focus:outline-none lg:hidden ${sidebarOpen ? 'hidden' : ''}`}>
-            <Bars3Icon className="h-6 w-6 text-white" />
-          </button>
+    return (
+        <div className="flex h-screen bg-gray-100">
+            <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} handleSectionClick={handleSectionClick} selectedSection={selectedSection} />
 
-          {/* Authentication Buttons */}
-          <div className="flex items-center justify-center ml-auto">
-            {user ? (
-              <>
-                <button onClick={handleMyAccountClick} className="px-3 py-2 border border-white rounded text-white hover:bg-yellow-300 hover:text-black no-underline hover:scale-110">My Account</button>
-                <button onClick={handleLogout} className="px-3 py-2 ml-2 border border-white rounded text-white hover:bg-yellow-300 hover:text-black hover:scale-110">Logout</button>
-              </>
-            ) : (
-              <>
-                <Link to="/login" className="px-3 py-2 border border-white rounded text-white hover:bg-yellow-300 hover:text-black no-underline hover:scale-110">Login</Link>
-                <Link to="/signup" className="px-3 py-2 ml-2 border border-white rounded text-white hover:bg-yellow-300 hover:text-black no-underline hover:scale-110">Signup</Link>
-              </>
-            )}
-          </div>
+            <div className="flex-1 flex flex-col overflow-hidden">
+                <header className="flex justify-between items-center p-4 text-white z-10" style={{ backgroundColor: '#FFDE59' }}>
+                    {/* Hamburger Icon */}
+                    <button onClick={() => setSidebarOpen(!sidebarOpen)} className={`focus:outline-none lg:hidden ${sidebarOpen ? 'hidden' : ''}`}>
+                        <Bars3Icon className="h-6 w-6 text-white" />
+                    </button>
 
-          {/* Right-aligned Dbarlist */}
-          <div className="hidden lg:flex items-center ml-auto">
-            <Dbarlist />
-          </div>
+                    {/* Authentication Buttons */}
+                    <div className="flex items-center justify-center ml-auto">
+                        {user ? (
+                            <>
+                                <button onClick={handleMyAccountClick} className="px-3 py-2 border border-white rounded text-white hover:bg-yellow-300 hover:text-black no-underline hover:scale-110">My Account</button>
+                                <button onClick={handleLogout} className="px-3 py-2 ml-2 border border-white rounded text-white hover:bg-yellow-300 hover:text-black hover:scale-110">Logout</button>
+                            </>
+                        ) : (
+                            <>
+                                <Link to="/login" className="px-3 py-2 border border-white rounded text-white hover:bg-yellow-300 hover:text-black no-underline hover:scale-110">Login</Link>
+                                <Link to="/signup" className="px-3 py-2 ml-2 border border-white rounded text-white hover:bg-yellow-300 hover:text-black no-underline hover:scale-110">Signup</Link>
+                            </>
+                        )}
+                    </div>
 
-          {/* Menu icon for smaller screens with added margin-left */}
-          <button onClick={() => setMenuOpen(!menuOpen)} className="lg:hidden focus:outline-none ml-4" ref={menuIconRef}>
-            <EllipsisVerticalIcon className="h-6 w-6 text-white" />
-          </button>
-        </header>
+                    {/* Right-aligned Dbarlist */}
+                    <div className="hidden lg:flex items-center ml-auto">
+                        <Dbarlist />
+                    </div>
 
-        {/* Menu popup */}
-        {menuOpen && <MPopper open={menuOpen} anchorRef={menuIconRef.current} handleClose={() => setMenuOpen(false)} handleListKeyDown={() => {}} />}
+                    {/* Menu icon for smaller screens with added margin-left */}
+                    <button onClick={() => setMenuOpen(!menuOpen)} className="lg:hidden focus:outline-none ml-4" ref={menuIconRef}>
+                        <EllipsisVerticalIcon className="h-6 w-6 text-white" />
+                    </button>
+                </header>
 
-        <main className="flex-1 overflow-y-auto p-2">
-          <Routes>
-            <Route path="/" index element={<Intro />} />
-            <Route path="home" index element={<Intro />} />
-            <Route path="search" element={<Scontent />} />
-            <Route path="ebooks" element={<Ebooks />} />
-            <Route path="ebooks/:id" element={<EbookDetails />} />
-            <Route path="audiobooks" element={<Audiobooks />} />
-            <Route path="audiobooks/:id" element={<AudiobookDetail />} />
-            <Route path="chapters/:id" element={<ChapterDetail />} />
-            <Route path="chapters" element={<Chapters />} />
-            <Route path="audiochapters" element={<AudioChapters />} />
-          </Routes>
-          <Footer />
-        </main>
-      
-      </div>
-    </div>
-  );
+                {/* Menu popup */}
+                {menuOpen && <MPopper open={menuOpen} anchorRef={menuIconRef.current} handleClose={() => setMenuOpen(false)} handleListKeyDown={() => { }} />}
+
+                <main className="flex-1 overflow-y-auto p-2">
+                    <Routes>
+                        <Route path="/" index element={<Intro />} />
+                        <Route path="home" index element={<Intro />} />
+                        <Route path="search" element={<Scontent />} />
+                        <Route path="ebooks" element={<Ebooks />} />
+                        <Route path="ebooks/:id" element={<EbookDetails />} />
+                        <Route path="audiobooks" element={<Audiobooks />} />
+                        <Route path="audiobooks/:id" element={<AudiobookDetail />} />
+                        <Route path="chapters/:id" element={<ChapterDetail />} />
+                        <Route path="chapters" element={<Chapters />} />
+                        <Route path="audiochapters" element={<AudioChapters />} />
+                    </Routes>
+                    <Footer />
+                </main>
+
+            </div>
+        </div>
+    );
 }
 
 export default Home;
