42291a89633718778d4221854ea04f9d0f526491
iMac <imac@iMacs-iMac.local>
2024-02-20 15:32:57 +0300

    api fix

--- src/pages/AudiobookDetailPage.js
+++ src/pages/AudiobookDetailPage.js
@@ -18,7 +18,7 @@ import { Helmet } from 'react-helmet';
       useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch(`http://localhost:3000/getAudiobook/${id}`);
+                const response = await fetch(`https://yeeplatformbackend.azurewebsites.net/getAudiobook/${id}`);
                 if (!response.ok) {
                     throw new Error('Ebook not found');
                 }
