020e40a648db03edc08e5b747fd9de4dd0ed7338
Nkugwa Mark William <nkugwamarkwilliam.com>
2024-07-13 01:27:10 +0300

    trying to make image load faster in loading screen

--- src/components/loadingscreen.js
+++ src/components/loadingscreen.js
@@ -1,20 +1,19 @@
-import React from "react";
-
+import React, { useEffect } from "react";
 
 const LoadingScreen = () => {
+  useEffect(() => {
+    const img = new Image();
+    img.src = "https://assets-hfbubwfaacbch3e0.z02.azurefd.net/assets/images/Y.webp";
+  }, []);
+
   return (
-    <>
     <div className="flex flex-col justify-center items-center h-screen">
       <img 
         src="https://assets-hfbubwfaacbch3e0.z02.azurefd.net/assets/images/Y.webp" 
         alt="loading" 
         className="w-1/4 animate-pulse md:w-1/6 mb-4"
       />
-
-    
     </div>
-   
-      </>
   );
 };
 
